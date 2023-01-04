9235e779674faba61c4032136eb4d9409d800d62
YlaneT <yloo318@yahoo.fr>
2023-01-04 16:17:32 +0100

    Fix player toString

--- src/main/java/Models/Entities/Player.java
+++ src/main/java/Models/Entities/Player.java
@@ -31,10 +31,10 @@ public class Player extends Character {
 		sb.append(UtilMenu.separate("Level", getLevel())).append("\n");
 		sb.append(UtilMenu.separate("Attack damage", getCurrentStatByName(ATTACK_DAMAGE))).append("\n");
 		sb.append(UtilMenu.separate("Ability power", getCurrentStatByName(ABILITY_POWER))).append("\n");
-		sb.append(UtilMenu.separate("Health", getCurrentStatByName(HEALTH))).append("\n");
+		sb.append(UtilMenu.separate("Max Health", getCurrentStatByName(MAX_HEALTH))).append("\n");
 		sb.append(UtilMenu.separate("Armor", getCurrentStatByName(ARMOR))).append("\n");
 		sb.append(UtilMenu.separate("Magic resistance", getCurrentStatByName(MAGIC_RESISTANCE))).append("\n");
-		sb.append(UtilMenu.separate("Mana", getCurrentStatByName(MANA))).append("\n");
+		sb.append(UtilMenu.separate("Max Mana", getCurrentStatByName(MAX_MANA))).append("\n");
 		sb.append("=".repeat(50)).append("\n");
 		return sb.toString();
 	}
